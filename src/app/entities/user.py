3ef2cb064dac6a0f6f4d2f3a0041b428f459c67e
nikolasfunke <nikfunke1@gmail.com>
2025-05-07 20:16:16 -0300

    feat: added entitie user and transaction

---
+++ src/app/entities/user.py
@@ -0,0 +1,14 @@
+class User:
+    def __init__(self, name : str, agency: str, account: str, current_balance:float):
+        if len(agency) != 4 or not agency.isdigit():
+            raise ValueError("Agencia deve conter exatamente 4 dígitos.")
+        
+        parts = account.split("-")
+        if len(parts) != 2 or not (parts[0].isdigit() and len(parts[0])) == 5 and parts[1].isdigit() and len(parts[1]) == 1:
+            raise ValueError("Conta deve coter 5 dígitos no formato XXXX-X")
+
+
+        self.name = name
+        self.agency = agency
+        self.account = account
+        self.current_balance = current_balance
