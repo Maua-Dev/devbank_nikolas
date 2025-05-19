82b6706b856f3ab0a2eb6c3f6bb65215dd93439f
nikolasfunke <nikfunke1@gmail.com>
2025-05-19 19:09:55 -0300

    Update: transaction entitie

--- src/app/entities/Transaction.py
+++ src/app/entities/Transaction.py
@@ -2,18 +2,21 @@ from datetime import datetime
 from typing import Dict
 from typing import Tuple
 from ..errors.entity_errors import ParamNotValidated
-
+from ..enums.transaction_type_enum import TransactionTypeEnum
 
 
 
 class Transactions:
-    timestamp: float
+    transaction_type = TransactionTypeEnum
+    bills: Dict[str, int]
+    timestamp: str
+    current_balance: float
     current_balance: float
 
-    def __init__(self, timestamp: float=None, current_balance: float=None, bills: Dict[int, int]=None, trnsaction_type: str=None):
+    def __init__(self, timestamp: str=None, current_balance: float=None, bills: Dict[str, int]=None, trnsaction_type: str=None):
         self.bills = bills
-        self.ammount = sum(note * qty for note, qty in bills.items())
-
+        self.ammount = sum(bill * qty for bill, qty in bills.items())
+        self.trnsaction_type = trnsaction_type
         if trnsaction_type == "deposit":
             self.current_balance = current_balance + self.ammount
         elif trnsaction_type == "withdraw":
@@ -25,3 +28,9 @@ class Transactions:
             raise ValueError("Tipo de trnsação inválido")
 
         self.timestamp = int(datetime.timestamp(datetime.now()) * 1000)
+
+    @staticmethod
+    def validate_bills(bills: Dict[str, int]) -> Tuple[bool, str]:
+        if bills is None:
+            return(False, "Bills is required")
+        
